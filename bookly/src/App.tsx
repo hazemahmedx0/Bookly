644f5c95a6a2525db7f120032d63f8dc6f58f5b1
Hazem <hazem31009444@f-eng.tanta.edu.eg>
2023-11-03 17:52:07 +0200

    Modify input and label

--- bookly/src/App.tsx
+++ bookly/src/App.tsx
@@ -1,9 +1,33 @@
+import { HashRouter, Route, Routes } from 'react-router-dom'
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+
 function App() {
     return (
         <>
-            <h1 className=" font-bold underline">Hello world!</h1>
+            <Button>Click me</Button>
+
+            <div className="grid w-full max-w-sm items-center gap-1.5 pl-2">
+                <Label htmlFor="email">Email</Label>
+                <Input type="email" id="email" placeholder="Email" />
+            </div>
+
+            <p className="text-display-X-small">Create an account</p>
+            <p>Tedtrfd</p>
+            <Routes>
+                <Route path="/" element={<p>fdd</p>} />
+            </Routes>
         </>
     )
 }
 
-export default App
+function WrappedApp() {
+    return (
+        <HashRouter>
+            <App />
+        </HashRouter>
+    )
+}
+
+export default WrappedApp
--- bookly/vite.config.ts
+++ bookly/vite.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import eslint from 'vite-plugin-eslint'
+
+import path from 'path'
+
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
@@ -9,4 +12,9 @@ export default defineConfig({
             exclude: ['/virtual:/**', 'node_modules/**'],
         }),
     ],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+        },
+    },
 })
